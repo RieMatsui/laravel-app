00f6302c795b98e92e6306ee7edb72dde5e3e1ea
RieMatsui <funkybass0405@gmail.com>
2019-11-25 00:43:32 +0900

    fixed TaskController and get Folder linked to task.

--- sample/app/Http/Controllers/TaskController.php
+++ sample/app/Http/Controllers/TaskController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
   public function index(int $id)
   {
+    // get all folder
     $folders = Folder::all();
+    //get current folder
+    $current_folder = Folder::find($id);
+    //get task on current folder
+    $tasks = Task::where('folder_id', $current_folder->id)->get();
     return view('tasks/index', [
       'folders' => $folders,
-      'current_folder_id' => $id,
+      'current_folder_id' => $current_folder->$id,
+      'task' => $tasks,
     ]);
   }
 }
