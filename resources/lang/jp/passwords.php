aadfc4e76d30ef5d1bc85093b418eeef190329ca
matsuirie <matsuirie@matsuirienoMacBook-Pro.local>
2020-01-07 03:18:29 +0900

    Close #19

---
+++ resources/lang/jp/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました！',
+    'sent' => 'パスワードリセット用のリンクを電子メールで送信しました！',
+    'token' => 'パスワードリセットトークンは無効です。',
+    'user' => "このメールアドレスを持つユーザーが見つかりません。",
+    'throttled' => '再試行する前にお待ちください。',
+
+];
